270a3dda24380341b284edba0241657d74900785
OverM12 <sitthisak2007.1@gmail.com>
2025-05-22 11:51:30 +0700

    Add Redirect Page

--- src/app/self/layout.js
+++ src/app/self/layout.js
@@ -1,7 +1,21 @@
+'use client';
 import Menu from "@/components/layout/Menu";
 import Navbar from "@/components/layout/Navbar";
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
+
 
 export default function RootLayout({ children }) {
+  const router = useRouter();
+
+	useEffect(() => {
+		const accessToken = localStorage.getItem('accessToken');
+
+		if (!accessToken) {
+			router.push('https://app-test.goodtime.app/login');
+		}
+	}, [router]);
+
   return (
     <html lang="en">
       <body className="">
