4f09e52097d20e5ebd4444ab095fa0c920f44107
OverM12 <sitthisak2007.1@gmail.com>
2025-05-22 11:52:31 +0700

    Verstion

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -319,7 +319,7 @@ function Menu() {
           </>
         )}
         <div>
-          <p className="text-white text-xs">v.1.0.1</p>
+          <p className="text-white text-xs">v.1.0.3</p>
         </div>
       </div>
     </>
